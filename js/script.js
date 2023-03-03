3b6f56ba18eeda0f57975dd88850aa70347550a7
Nataliia Martynenko <87510nata@gmail.com>
2023-03-03 10:13:20 +0100

    lesson 19

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 'use strict';
 
-// alert('Hello');
-const category = 'toys';
-console.log(4 + +'5');
+let numberOfFilms=+prompt('How many films did you watch?', '');
+
+let personalMovieDB={
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+
+let watchedFilm=prompt('Name of the random film watched?');
+let rate=prompt('Your rate for this film?');
+
+let watchedFilm1=prompt('Name of the random film watched?');
+let rate1=prompt('Your rate for this film?');
+personalMovieDB.movies[watchedFilm]=rate;
+personalMovieDB.movies[watchedFilm1]=rate1;
+console.log(personalMovieDB);
