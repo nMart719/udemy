65f0e6dd8893653651132e8ff0ce84f658e5b785
Nataliia Martynenko <87510nata@gmail.com>
2023-03-04 13:57:14 +0100

    lesson 21

--- js/script.js
+++ js/script.js
@@ -1,29 +1,48 @@
 'use strict';
 
-const num = '50';
-// if( num<49 ){
-// 	console.log('error');
-// }else if (num>100){
-// 	console.log('Too much');
-// }else{
-// 	console.log('ok');
+// let hamburger = 5;
+// let fries = 2;
+// let cola = 1;
+
+// console.log('hamburger == 5 ' + (hamburger == 5));
+// console.log('cola == 1 ' + (cola == 1));
+// console.log('cola ' + cola);
+// console.log('hamburger == 5 && cola ' + (hamburger == 5 && cola));
+// console.log('hamburger == 5 && fries ' + (hamburger == 5 && fries));
+// console.log('hamburger && fries ' + (hamburger && fries));
+// console.log('fries && hamburger ' + (fries && hamburger));
+// console.log('fries && \'hamburger\' ' + (fries && 'hamburger'));
+// console.log('0 && \'hamburger\' ' + (0 && 'hamburger'));
+// console.log('\'hamburger\' && 0 ' + ('hamburger' && 0));
+// console.log('hamburger && 0 ' + (hamburger && 0));
+// console.log('false && \'hamburger\' ' + (false && 'hamburger'));
+// console.log('\'hamburger\' && false ' + ('hamburger' && false));
+// console.log('hamburger && false ' + (hamburger && false));
+// console.log('null && \'hamburger\' ' + (null && 'hamburger'));
+// console.log(typeof(''));
+// console.log('\'\' && true' + ('' && true));
+// console.log('\'\' && false' + ('' && false));
+// console.log('\'\' && 1' + ('' && 1));
+// console.log('\'\' && 0' + ('' && 0));
+// console.log('\'\' && null' + ('' && null));
+// console.log('\'\' && undefined' + ('' && undefined));
+// console.log('\'\' && \'\'' + ('' && ''));
+
+// console.log(typeof(!1));
+// console.log(typeof(!0));
+// console.log(typeof(!null));
+// console.log(typeof(!undefined));
+
+
+// console.log(!'undefined');
+// console.log(!'');
+// console.log(typeof(!'undefined'));
+// console.log(typeof(!''));
+
+
+// console.log(fries==2);
+
+// if( hamburger && fries){
+// 	console.log('full');
 // }
-
-// ( num === 50 ) ? console.log('ok') : console.log('error');
-
-//switch is a ===
-switch(num){
-case '49':
-	console.log('no');
-	break;
-case '100':
-	console.log('too much');
-	break;
-case '50':
-	console.log('close');
-	break;
-default:
-	console.log('maybe');
-	break;
-
-}
+// console.log(hamburger&&fries);
