92ba43628b03ea76d2664a3f4c722a263e5df4c4
Nataliia Martynenko <87510nata@gmail.com>
2023-03-04 15:06:52 +0100

    lesson 22 + tasks

--- js/script.js
+++ js/script.js
@@ -1,48 +1,45 @@
 'use strict';
-
-// let hamburger = 5;
-// let fries = 2;
-// let cola = 1;
-
-// console.log('hamburger == 5 ' + (hamburger == 5));
-// console.log('cola == 1 ' + (cola == 1));
-// console.log('cola ' + cola);
-// console.log('hamburger == 5 && cola ' + (hamburger == 5 && cola));
-// console.log('hamburger == 5 && fries ' + (hamburger == 5 && fries));
-// console.log('hamburger && fries ' + (hamburger && fries));
-// console.log('fries && hamburger ' + (fries && hamburger));
-// console.log('fries && \'hamburger\' ' + (fries && 'hamburger'));
-// console.log('0 && \'hamburger\' ' + (0 && 'hamburger'));
-// console.log('\'hamburger\' && 0 ' + ('hamburger' && 0));
-// console.log('hamburger && 0 ' + (hamburger && 0));
-// console.log('false && \'hamburger\' ' + (false && 'hamburger'));
-// console.log('\'hamburger\' && false ' + ('hamburger' && false));
-// console.log('hamburger && false ' + (hamburger && false));
-// console.log('null && \'hamburger\' ' + (null && 'hamburger'));
-// console.log(typeof(''));
-// console.log('\'\' && true' + ('' && true));
-// console.log('\'\' && false' + ('' && false));
-// console.log('\'\' && 1' + ('' && 1));
-// console.log('\'\' && 0' + ('' && 0));
-// console.log('\'\' && null' + ('' && null));
-// console.log('\'\' && undefined' + ('' && undefined));
-// console.log('\'\' && \'\'' + ('' && ''));
-
-// console.log(typeof(!1));
-// console.log(typeof(!0));
-// console.log(typeof(!null));
-// console.log(typeof(!undefined));
-
-
-// console.log(!'undefined');
-// console.log(!'');
-// console.log(typeof(!'undefined'));
-// console.log(typeof(!''));
-
-
-// console.log(fries==2);
-
-// if( hamburger && fries){
-// 	console.log('full');
+// let str='';
+// for (let i = 0; i < 7; i++) {	
+// 	for (let j = 0; j < i; j++) {
+// 		str=str+'*';		
+// 	}
+// 	str=str +'\n';
 // }
-// console.log(hamburger&&fries);
+// console.log(str);
+// let str='';
+// for (let i = 0; i < 7; i++) {	
+// 	for (let j = 0; ; j++) {
+// 		if (j>=i) break;
+// 		str=str+'*';		
+// 	}
+// 	str=str +'\n';
+// }
+// console.log(str);
+
+
+// f:for (let i = 0; i < 3; i++) {	
+// 	console.log('First: '+ i);
+// 	for (let j = 0; j < 3; j++) {
+// 		console.log('\tSecond: '+ j);
+// 		for (let k = 0; k < 3; k++) {
+// 			if (k === 2) continue f;
+// 			console.log('\t\tThird: '+ k);
+// 		}
+// 	}
+// }
+
+
+
+const lines = 5;
+let result = '';
+let stars=-1;
+for (let i = 0; i <= lines; i++) {
+	stars+=2;	
+	for (let j = 6+i; j > 0; j--) {
+		if(j>stars) result+='-';
+		else result+='*';
+	}
+	result+='\n';
+}
+console.log(result);
