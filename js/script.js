ef5340ce7101c0a16298f0a7d938c3dcbb9afa6c
Nataliia Martynenko <87510nata@gmail.com>
2023-03-05 16:01:00 +0100

    lesson 25

--- js/script.js
+++ js/script.js
@@ -1,55 +1,21 @@
 'use strict';
 
-
-let numberOfFilms=+prompt('How many films did you watch?', '');
-
-let personalMovieDB={
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
-
-
-
-for (let i = 0; i < 2; i++) {
-	let watchedFilm=prompt('Name of the random film watched?');
-	while(watchedFilm === '' || watchedFilm === null || watchedFilm.length>=50){	
-		watchedFilm=prompt('Name of the random film watched?');
-	}
-	let rate=prompt('Your rate for this film?');
-
-	personalMovieDB.movies[watchedFilm]=rate;
-	
+console.log(calc(1,2));
+function calc(a,b){
+	return a+b;
 }
 
-// let i=0;
-// do {
-// 	let watchedFilm=prompt('Name of the random film watched?');
-// 	while(watchedFilm === '' || watchedFilm === null || watchedFilm.length>=50){	
-// 		watchedFilm=prompt('Name of the random film watched?');
-// 	}
-// 	let rate=prompt('Your rate for this film?');
-// 	personalMovieDB.movies[watchedFilm]=rate;
-// 	i++;
-// }while(i<1);
-
-// let i=0;
-// while (i < 2 ) {
-// 	let watchedFilm=prompt('Name of the random film watched?');
-// 	while(watchedFilm === '' || watchedFilm === null || watchedFilm.length>=50){	
-// 		watchedFilm=prompt('Name of the random film watched?');
-// 	}
-// 	let rate=prompt('Your rate for this film?');
-// 	personalMovieDB.movies[watchedFilm]=rate;
-// 	i++;
-// }
 
 
-if(personalMovieDB['count']<10) console.log('You\'ve watched pretty poor number of films');
-else if(10<=personalMovieDB['count'] && personalMovieDB['count']<=30) console.log('You are classic viewer');
-else if(personalMovieDB['count']>30) console.log('You are a cinephile');
-else console.log('Error');
+const logger = function(){
+	return(7);
+};
 
-console.log(personalMovieDB);
+console.log(typeof(logger()));
+
+let calcArrow = (a,b) =>{return a+b;};
+let calcArrowShort = (a,b) =>a+b;
+
+
+console.log((calcArrow(1,2)));
+console.log(typeof(calcArrow(1,2)));
