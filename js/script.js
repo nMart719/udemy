3517becf6499fb7f281f695e5f756690dffdaff1
Nataliia Martynenko <87510nata@gmail.com>
2023-03-07 16:58:22 +0100

    lesson 29, practise pt3

--- js/script.js
+++ js/script.js
@@ -1,21 +1,71 @@
 'use strict';
 
-console.log(calc(1,2));
-function calc(a,b){
-	return a+b;
+let numberOfFilms;
+
+function start(){
+	numberOfFilms=+prompt('How many films did you watch?', '');
+	while(numberOfFilms === '' || numberOfFilms === null || isNaN(numberOfFilms)){	
+		numberOfFilms=+prompt('How many films did you watch?', '');
+	}
 }
+start();
 
-
-
-const logger = function(){
-	return(7);
+let personalMovieDB={
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
 };
 
-console.log(typeof(logger()));
-
-let calcArrow = (a,b) =>{return a+b;};
-let calcArrowShort = (a,b) =>a+b;
+function remeberMyFilms(){
+	let result = '';
 
 
-console.log((calcArrow(1,2)));
-console.log(typeof(calcArrow(1,2)));
+	for (let i = 0; i < 2; i++) {
+		let watchedFilm=prompt('Name of the random film watched?');
+		while(watchedFilm === '' || watchedFilm === null || watchedFilm.length>=50){	
+			watchedFilm=prompt('Name of the random film watched?');
+		}
+		result+='\n';
+		let rate=prompt('Your rate for this film?');
+	
+		personalMovieDB.movies[watchedFilm]=rate;
+	
+	}
+	console.log(result);
+}
+ 
+remeberMyFilms();
+function detectPersonalLevel(){
+
+	if(personalMovieDB['count']<10) console.log('You\'ve watched pretty poor number of films');
+	else if(10<=personalMovieDB['count'] && personalMovieDB['count']<=30) console.log('You are classic viewer');
+	else if(personalMovieDB['count']>30) console.log('You are a cinephile');
+	else console.log('Error');
+
+
+}
+detectPersonalLevel();
+
+
+function showMyDB(){
+	if (!personalMovieDB['privat']) console.log(personalMovieDB);
+
+}
+showMyDB();
+
+function writeYourGenres(){
+
+	for (let i = 0; i < 3; i++) {
+		let genre=prompt(`Your favourite genre of film by number ${i+1}?`);
+		while(genre === '' || genre === null || genre.length>=40){	
+			genre=prompt(`Your favourite genre of film by number ${i+1}?`);
+		}
+	
+		personalMovieDB.genres[i]=genre;
+		console.log(personalMovieDB.genres);
+	}
+}
+
+writeYourGenres();
