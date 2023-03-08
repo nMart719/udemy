4d58c4300411a2d681c2e9ca6d04eaa647e96a32
Nataliia Martynenko <87510nata@gmail.com>
2023-03-08 14:42:56 +0100

    fibonacci numbers

--- js/script.js
+++ js/script.js
@@ -1,71 +1,30 @@
-'use strict';
-
-let numberOfFilms;
-
-function start(){
-	numberOfFilms=+prompt('How many films did you watch?', '');
-	while(numberOfFilms === '' || numberOfFilms === null || isNaN(numberOfFilms)){	
-		numberOfFilms=+prompt('How many films did you watch?', '');
+function fib(num) {
+	if(isNaN(num) || !Number.isInteger(num) || num<=0){
+		return '';
 	}
-}
-start();
-
-let personalMovieDB={
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
-
-function remeberMyFilms(){
-	let result = '';
-
-
-	for (let i = 0; i < 2; i++) {
-		let watchedFilm=prompt('Name of the random film watched?');
-		while(watchedFilm === '' || watchedFilm === null || watchedFilm.length>=50){	
-			watchedFilm=prompt('Name of the random film watched?');
+	let result='0 1 ';
+	let reallyBefore;
+	let before;
+	let temp;
+	if(num===0) return '';
+	else if (num===1) return '0';
+	for (var i = 0; i < num; i++) {
+		if(i>1){
+        
+			reallyBefore=parseInt(result[result.length-4]);
+			before=parseInt(result[result.length-2]);
+			temp=reallyBefore+before;
+			console.log(`${reallyBefore}[${result[result.length-4]}]+${before}[${result.length-2}]=${temp}`);
+			result += `${temp} `;
+			console.log('result: '+result);
 		}
-		result+='\n';
-		let rate=prompt('Your rate for this film?');
-	
-		personalMovieDB.movies[watchedFilm]=rate;
-	
 	}
-	console.log(result);
-}
- 
-remeberMyFilms();
-function detectPersonalLevel(){
-
-	if(personalMovieDB['count']<10) console.log('You\'ve watched pretty poor number of films');
-	else if(10<=personalMovieDB['count'] && personalMovieDB['count']<=30) console.log('You are classic viewer');
-	else if(personalMovieDB['count']>30) console.log('You are a cinephile');
-	else console.log('Error');
-
-
-}
-detectPersonalLevel();
-
-
-function showMyDB(){
-	if (!personalMovieDB['privat']) console.log(personalMovieDB);
-
-}
-showMyDB();
-
-function writeYourGenres(){
-
-	for (let i = 0; i < 3; i++) {
-		let genre=prompt(`Your favourite genre of film by number ${i+1}?`);
-		while(genre === '' || genre === null || genre.length>=40){	
-			genre=prompt(`Your favourite genre of film by number ${i+1}?`);
-		}
-	
-		personalMovieDB.genres[i]=genre;
-		console.log(personalMovieDB.genres);
-	}
+	return result.substring(0,result.length-1);
 }
 
-writeYourGenres();
+// console.log(fib(0));
+// console.log(fib());
+//  console.log(fib(''));
+// console.log(fib('s'));
+console.log(fib(1));
+console.log(fib(5));
