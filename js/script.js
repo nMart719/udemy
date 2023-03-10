34dc80fefe306c03275466e79524c91fa183ff10
Nataliia Martynenko <87510nata@gmail.com>
2023-03-10 10:47:13 +0100

    lesson 32

--- js/script.js
+++ js/script.js
@@ -1,30 +1,29 @@
-function fib(num) {
-	if(isNaN(num) || !Number.isInteger(num) || num<=0){
-		return '';
+'use strict';
+let options={
+	name: 'name',
+	width: 1024,
+	height:1024,
+	colors: {
+		border: 'black',
+		bg:'red'
+	},
+	makeTest: function(){
+		console.log('Test');
 	}
-	let result='0 1 ';
-	let reallyBefore;
-	let before;
-	let temp;
-	if(num===0) return '';
-	else if (num===1) return '0';
-	for (var i = 0; i < num; i++) {
-		if(i>1){
-        
-			reallyBefore=parseInt(result[result.length-4]);
-			before=parseInt(result[result.length-2]);
-			temp=reallyBefore+before;
-			console.log(`${reallyBefore}[${result[result.length-4]}]+${before}[${result.length-2}]=${temp}`);
-			result += `${temp} `;
-			console.log('result: '+result);
-		}
-	}
-	return result.substring(0,result.length-1);
-}
+};
+// options.makeTest();
+// console.log(Object.keys(options).length);
+// console.log(options.length);
 
-// console.log(fib(0));
-// console.log(fib());
-//  console.log(fib(''));
-// console.log(fib('s'));
-console.log(fib(1));
-console.log(fib(5));
+// for (const key in options) {
+// 	if(typeof(options[key])==='object'){
+// 		for (const i in options[key]) {
+// 			console.log(`Property ${i} has value ${options[key][i]}`);
+// 		}
+// 	} else{
+// 		console.log(`Property ${key} has value ${options[key]}`);
+// 	}
+// }
+
+const {border,bg}=options.colors;
+console.log(border);
