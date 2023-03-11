3bcb55bf3dbdf962cbaecff1d5cf6e666fe28e53
Nataliia Martynenko <87510nata@gmail.com>
2023-03-11 10:07:13 +0100

    lesson 35

--- js/script.js
+++ js/script.js
@@ -1,29 +1,87 @@
 'use strict';
-let options={
-	name: 'name',
-	width: 1024,
-	height:1024,
-	colors: {
-		border: 'black',
-		bg:'red'
-	},
-	makeTest: function(){
-		console.log('Test');
+let a=5, b=a;
+b=b+5;
+
+const obj={
+	a: 5,
+	b: 1
+};
+
+
+
+function copy(mainObj){
+	let objCopy={};
+	let key;
+	for (key in mainObj) {
+		objCopy[key]=mainObj[key];
+	}
+	return objCopy;
+}
+
+
+const numbers = {
+	a:2,
+	b:5,
+	c: {
+		x: 7,
+		y: 4
 	}
 };
-// options.makeTest();
-// console.log(Object.keys(options).length);
-// console.log(options.length);
 
-// for (const key in options) {
-// 	if(typeof(options[key])==='object'){
-// 		for (const i in options[key]) {
-// 			console.log(`Property ${i} has value ${options[key][i]}`);
-// 		}
-// 	} else{
-// 		console.log(`Property ${key} has value ${options[key]}`);
-// 	}
-// }
+const newNumbers = copy(numbers);
 
-const {border,bg}=options.colors;
-console.log(border);
+
+
+const add = {
+	d:17,
+	e:20
+};
+const clone = Object.assign({},add);
+clone.d = 20;
+// console.log(add);
+// console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1]='bla';
+// console.log(oldArray);
+// console.log(newArray);
+
+
+const video = ['youtube', 'vimeo', 'uatube'],
+	blogs = ['wordpress', 'livejournal', 'blogger'],
+	internet = [...video, ...blogs, 'vk', 'facebook'];
+
+// blogs[0]='a';
+internet[0]='b';
+console.log(video);
+console.log(blogs);
+console.log(internet);
+
+
+function log(a,b,c){
+	console.log(a);
+	console.log(b);
+	console.log(c);
+}
+
+const num = [2,5,7];
+log(...num);
+
+
+const array = ['a', 'b'];
+const newAarray = [...array];
+
+const q= {
+	one: 1,
+	two: 2
+};
+
+const newObject = {
+	...q
+};
+
+newObject['one'] = 'one';
+console.log(q);
+console.log(newObject);
