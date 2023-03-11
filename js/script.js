1a7bc843428d45b81db2e182fa29f8e5295c5754
Nataliia Martynenko <87510nata@gmail.com>
2023-03-12 00:00:04 +0100

    lesson 36

--- js/script.js
+++ js/script.js
@@ -1,46 +1,29 @@
 'use strict';
-const someString = 'This is some strange string';
-
-function reverse(str) {
-	if(typeof(str)!=='string') return 'Ошибка!';
-	return str.split(' ').reverse().join(' ');
-	
-}
+let str = 'some';
+let strObj=new String(str);
 
 
-reverse(someString);
-reverse('');
-reverse();
-reverse(3);
+// console.log(typeof(str));
+// console.log(typeof(strObj));
 
-const baseCurrencies = ['USD', 'EUR'];
-const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
 
-function availableCurr(arr, missingCurr) {
-	
-	if(arr.length<=0) return 'Нет доступных валют';
-	
-	for (let j=0 ; j<missingCurr.length; j++) {	
-		for (let i=0; i< arr.length; i++) {
-			if(arr[i]===missingCurr[j]) delete arr[i];
-			
-		}
-		
+// console.dir([1,2,3]);
+
+
+const soldier = {
+	health: 400,
+	armor: 100,
+	sayHello: function(){
+		console.log('Hello');
 	}
-	let str='Доступные валюты:';
-	for (let i=0; i< arr.length; i++) { 
-		if(arr[i]!== undefined){
-			str+=`\n${arr[i]}`;
-		}
-	}
-	return str;
-}
+};
 
+const john = {
+	health: 200
+};
+Object.setPrototypeOf(john,soldier);// == john.__proto__=soldier;
+console.log(john.sayHello());
 
-console.log(availableCurr('',additionalCurrencies));
-console.log('!!!!!!!!!!!!');
-console.log(availableCurr(baseCurrencies,additionalCurrencies));
-console.log('!!!!!!!!!!!!');
-console.log(availableCurr(['UAH', 'RUB', 'CNY'],['CNY']));
-console.log('!!!!!!!!!!!!');
-console.log(availableCurr(['UAH', 'RUB', 'CNY','USD', 'EUR'],['USD', 'EUR','CNY']));
+const sam = Object.create(soldier);
+
+console.log(john.sayHello());
