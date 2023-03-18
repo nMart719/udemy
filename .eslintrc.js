6cd9bdefd3af0f0ced865c7640334582ef99b515
Nataliia Martynenko <87510nata@gmail.com>
2023-03-18 10:41:57 +0100

    lesson 42

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,10 +15,7 @@ module.exports = {
 			'warn',
 			'tab'
 		],
-		'linebreak-style': [
-			'error',
-			'windows'
-		],
+		'linebreak-style': 0,
 		'quotes': [
 			'error',
 			'single'
--- js/script.js
+++ js/script.js
@@ -1,86 +1,54 @@
-'use strict';
-
-
-
-
-
-
-
-let personalMovieDB={
-	count: 0,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false,
-	start: function(){
-		personalMovieDB.count=+prompt('How many films did you watch?', '0');
-		while(personalMovieDB.count === '' || personalMovieDB.count === null || isNaN(personalMovieDB.count)){	
-			personalMovieDB.count=+prompt('How many films did you watch?', '');
+const restorantData = {
+	menu: [
+		{
+			name: 'Salad Caesar',
+			price: '14$'
+		},
+		{
+			name: 'Pizza Diavola',
+			price: '9$'
+		},
+		{
+			name: 'Beefsteak',
+			price: '17$'
+		},
+		{
+			name: 'Napoleon',
+			price: '7$'
 		}
-	},
-	remeberMyFilms:function(){
-		let result = '';
-	
-		const logger = function(){
-			return(7);
-		};
-	
-		console.log(typeof(logger()));
-		for (let i = 0; i < 2; i++) {
-			let watchedFilm=prompt('Name of the random film watched?');
-			while(watchedFilm === '' || watchedFilm === null || watchedFilm.length>=50){	
-				watchedFilm=prompt('Name of the random film watched?');
-			}
-			result+='\n';
-			let rate=prompt('Your rate for this film?');
-	
-			personalMovieDB.movies[watchedFilm]=rate;
-	
-		}
-		console.log(result);
-	},
-	detectPersonalLevel: function(){
-
-		if(personalMovieDB['count']<10) console.log('You\'ve watched pretty poor number of films');
-		else if(10<=personalMovieDB['count'] && personalMovieDB['count']<=30) console.log('You are classic viewer');
-		else if(personalMovieDB['count']>30) console.log('You are a cinephile');
-		else console.log('Error');
-	
-	
-	},
-	showMyDB : function (){
-		if (!personalMovieDB['privat']) console.log(personalMovieDB);
-	
-	},
-	writeYourGenres: function(){
-
-		for (let i = 0; i < 3; i++) {
-			let genre=prompt(`Your favourite genre of film by number ${i+1}?`);
-			while(genre === '' || genre === null || genre.length>=40 || !genre){	
-				genre=prompt(`Your favourite genre of film by number ${i+1}?`);
-			}
-	
-			personalMovieDB.genres[i]=genre;
-			
-		}
-
-		personalMovieDB.genres.forEach(item => {
-			console.log(`Favourite genre by number #${personalMovieDB.genres.indexOf(item)+1} - this is ${item}`);
-		});
-	},
-	toggleVisibleMyDB: function(){
-		!personalMovieDB.privat ? personalMovieDB.privat=true : personalMovieDB.privat=false;
-	}
+	],
+	waitors: [
+		{name: 'Alice', age: 22}, {name: 'John', age: 24}
+	],
+	averageLunchPrice: '20$',
+	openNow: true
 };
 
+function isOpen(prop) {
+	let answer = '';
+	prop ? answer = 'Открыто' : answer = 'Закрыто';
 
-// personalMovieDB.start();
-// personalMovieDB.remeberMyFilms();
-// personalMovieDB.detectPersonalLevel();
-// personalMovieDB.showMyDB();
-// personalMovieDB.toggleVisibleMyDB();
-// personalMovieDB.showMyDB();
+	return answer;
+}
 
+console.log(isOpen(restorantData.openNow));
 
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+	let first=+fDish.price.slice(0, -1), second=+sDish.price.slice(0, -1);
+	if (+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)) < average) {
+		return 'Цена ниже средней';
+	} else {
+		return 'Цена выше средней';
+	}
+}
 
-personalMovieDB.writeYourGenres();
+console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
+
+function transferWaitors(data) {
+	const copy = Object.assign({}, data);
+
+	copy.waitors = {name: 'Mike', age: 32};
+	return copy;
+}
+
+transferWaitors(restorantData);
