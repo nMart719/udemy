6c73f157a267946af5eda01bab4883edee8247a7
Nataliia Martynenko <87510nata@gmail.com>
2023-03-20 10:48:15 +0100

    lesson 46 dom_elements

--- js/script.js
+++ js/script.js
@@ -1,54 +1,16 @@
-const restorantData = {
-	menu: [
-		{
-			name: 'Salad Caesar',
-			price: '14$'
-		},
-		{
-			name: 'Pizza Diavola',
-			price: '9$'
-		},
-		{
-			name: 'Beefsteak',
-			price: '17$'
-		},
-		{
-			name: 'Napoleon',
-			price: '7$'
-		}
-	],
-	waitors: [
-		{name: 'Alice', age: 22}, {name: 'John', age: 24}
-	],
-	averageLunchPrice: '20$',
-	openNow: true
-};
+'use strict';
+// console.log(document.head);	
+// console.log(document.body.childNodes);	
+// console.log(document.head.childNodes);	
+// console.log(document.head.firstChild);	
+console.log(document.body.childNodes);	
+// console.log(document.head.lastChild);	
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('#current').parentNode.parentElement);
+// console.log(document.querySelector('[data-current="3"').nextElementSibling);
 
-function isOpen(prop) {
-	let answer = '';
-	prop ? answer = 'Открыто' : answer = 'Закрыто';
-
-	return answer;
+//same as childElements
+for (const node of document.body.childNodes) {
+	if (node.nodeName=='#text') continue;
+	console.log(node);
 }
-
-console.log(isOpen(restorantData.openNow));
-
-function isAverageLunchPriceTrue(fDish, sDish, average) {
-	let first=+fDish.price.slice(0, -1), second=+sDish.price.slice(0, -1);
-	if (+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)) < average) {
-		return 'Цена ниже средней';
-	} else {
-		return 'Цена выше средней';
-	}
-}
-
-console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
-
-function transferWaitors(data) {
-	const copy = Object.assign({}, data);
-
-	copy.waitors = {name: 'Mike', age: 32};
-	return copy;
-}
-
-transferWaitors(restorantData);
