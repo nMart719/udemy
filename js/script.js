45625a49f4aa2566d7cdec0de996d71d1f80928d
Nataliia Martynenko <87510nata@gmail.com>
2023-03-24 22:47:02 +0100

    lesson 48, practice pt.5

--- js/script.js
+++ js/script.js
@@ -1,16 +1,2 @@
 'use strict';
-// console.log(document.head);	
-// console.log(document.body.childNodes);	
-// console.log(document.head.childNodes);	
-// console.log(document.head.firstChild);	
-console.log(document.body.childNodes);	
-// console.log(document.head.lastChild);	
-// console.log(document.querySelector('#current').parentNode.parentNode);
-// console.log(document.querySelector('#current').parentNode.parentElement);
-// console.log(document.querySelector('[data-current="3"').nextElementSibling);
 
-//same as childElements
-for (const node of document.body.childNodes) {
-	if (node.nodeName=='#text') continue;
-	console.log(node);
-}
