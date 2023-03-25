32e781d8ea353cfcc799a19a108ae793812b51bf
Nataliia Martynenko <87510nata@gmail.com>
2023-03-25 10:16:42 +0100

    lesson 49, mobile events

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 
 
 'use strict';
+'use strict';
+window.addEventListener('DOMContentLoaded', () =>{
+	
+	const box=document.querySelector('.box');
+	box.addEventListener('touchstart', (e) =>{
+		e.preventDefault();
+		console.log('Start');
+		console.log(e.changedTouches);
+	});
+	box.addEventListener('touchmove', (e) =>{
+		e.preventDefault();
+		console.log(e.targetTouches[0].pageX);
+	});
+	// box.addEventListener('touchend', (e) =>{
+	// 	e.preventDefault();
+	// 	console.log('End');
+	// });
+});
+
 
 // Возьмите свой код из предыдущей практики
