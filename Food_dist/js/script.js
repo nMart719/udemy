a8372eead438aa48ae729c95b21ba6043762cae0
Nataliia Martynenko <87510nata@gmail.com>
2023-03-27 12:32:12 +0200

    lesson 65, simple animation

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 window.addEventListener('DOMContentLoaded', () =>{
 	const tabs = document.querySelectorAll('.tabheader__item'),
 		tabsContent = document.querySelectorAll('.tabcontent'),
