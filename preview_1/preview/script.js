205bb9a6de7848665e35266352a906301e5714a4
Nataliia Martynenko <87510nata@gmail.com>
2023-03-31 09:39:15 +0200

    lesson 55

--- preview_1/preview/script.js
+++ preview_1/preview/script.js
@@ -1,17 +1,22 @@
 'use strict';
 
 const box = document.querySelector('.box');
-const newWidth=400, newHeight=100;
-
-function changeParams(elem, h, w){
-	elem.style.height=`${h ?? 200}px`;
-	elem.style.width=`${w ?? 200}px`;
-	elem.innerHTML = (h ?? 200) * (w ?? 200);
-}
 
 const block = document.querySelector('.block');
 
-changeParams(box, newHeight, newWidth);
+console.log(block);
+// if(block)console.log(block.textContent);
+// console.log(block?.textContent);// operator ? checks left side first and returns undefined if it's false
+//block?.textContent='123';// here we trying to initiate undefined as 123, but that's impossible
+console.log(1+2);
 
-let userName, userKey;
-console.log((userName && userKey) ?? 'User');
+const userData = {
+	name: 'Ivan',
+	age: null,
+	say: function(){
+		console.log('Hello');
+	}
+};
+userData.say();
+userData.hey?.();
+console.log(userData?.skills?.js);// = if(userData && userData.skills && userData.skills.js)
