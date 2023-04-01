69e340179d1254985d5493bda5196c9a4c8f4fd5
Nataliia Martynenko <87510nata@gmail.com>
2023-04-01 15:10:41 +0200

    lesson 58

--- preview_1/preview/script.js
+++ preview_1/preview/script.js
@@ -1,28 +1,60 @@
 'use strict';
-const birthday = Symbol('birthday');
 const user ={
 	name: 'Alex',
 	surname: 'Smith',
-	[birthday]: '20/04/2021',
+	birthday: '20/04/2021',
 	showMyPublicData: function(){
 		console.log(`${this.name} ${this.surname}`);
 	}
 };
 
-// Object.defineProperty(user,'birthday', {value: prompt('Date?',''),enumerable: true, configurable: false});
-// console.log(Object.getOwnPropertyDescriptor(user,'birthday'));
-// Object.defineProperty(user,'name', {writable: false});
-console.log(Object.getOwnPropertyDescriptor(Math,'PI'));
-Object.defineProperty(user,'showMyPublicData',{enumerable: false, configurable: false});
+// for (const key in user) {
+// 	console.log(user[key]);
+// }
+// const arr =['b','a','c'];
+// Array.prototype.someMethod = function(){};
+// console.dir(arr);
+// for (const key of arr) {
+// 	console.log(key);
+// }
+// const str ='string';
 
-for (const key in user) {
-	console.log(key);
-}
-console.log(Object.getOwnPropertyDescriptor(user, birthday));
-Object.defineProperties(user, {
-	name: {value: 'John',writable: false},
-	surname: {writable:false}
-});
+// for (const key of str) {
+// 	console.log(key);
+// }
 //writable
 //enumerable
 //configurable
+
+
+
+const salaries = {
+	john: 500,
+	ivan: 1000,
+	anna: 5000,
+	sayHello: function(){
+		console.log('Hello');
+	}
+
+
+};
+
+salaries[Symbol.iterator]=function(){
+	return {
+		current: this.john,
+		last: this.anna,
+		next(){
+			if(this.current<this.last) {
+				this.current+=500;
+				return {done: false, value: this.current};
+			}else{
+				return{done: true};
+			}
+		}
+	};
+};
+const iterator = salaries[Symbol.iterator]();
+console.log(iterator.next());
+for(let res of salaries){
+	console.log(res);
+}
