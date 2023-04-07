57d29391667d5e12eed89de33f1691013bc06d58
Nataliia Martynenko <87510nata@gmail.com>
2023-04-07 10:12:06 +0200

    lesson 61

--- preview_1/preview/script.js
+++ preview_1/preview/script.js
@@ -1,47 +1,8 @@
 'use strict';
-const user ={
-	name: 'Alex',
-	surname: 'Smith',
-	birthday: '20/04/2021',
-	showMyPublicData: function(){
-		console.log(`${this.name} ${this.surname}`);
-	}
-};
-
-
-
-const userMap = new Map(Object.entries(user));
-console.log(userMap);
-
-
-const newUserObj = Object.fromEntries(userMap);
-
-console.log(newUserObj);
-const shops = [
-	{rice: 500},
-	{oil: 200},
-	{bread: 50}
-];
-const budget=[5000,3000,2000];
-const map = new Map([
-	[{paper : 400},8000]
-]);
-shops.forEach((shop,i)=>{
-	map.set(shop, budget[i]);
-});
-// console.log(map.get(shops[0]));
-// console.log(map);
-// for (const [shop, price] of map.entries()) {
-// 	console.log(price, shop);
-// }
-
-
-// map.forEach( (value, key, map) => {
-// 	console.log(key, value);
-// });
-
-// const goods = [];
-// for(let shop of map.keys()){
-// 	goods.push(Object.keys(shop)[0]);
-// }
-// console.log(goods);
+//const bigInt = 12837284675913658623953546n;
+const sameBigInt = BigInt(12837284675913658623953546);
+//console.log(10n===10);
+const bigint=1n;
+const num=2;
+//console.log(bigint+BigInt(num));
+console.log(Number(bigint)+num);
