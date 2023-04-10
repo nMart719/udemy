d17f865ad74321c4ee77ff838e2fdad96ea68dae
Nataliia Martynenko <87510nata@gmail.com>
2023-04-10 15:40:19 +0200

    lesson 67

--- preview_1/preview/script.js
+++ preview_1/preview/script.js
@@ -1,8 +1,40 @@
 'use strict';
-//const bigInt = 12837284675913658623953546n;
-const sameBigInt = BigInt(12837284675913658623953546);
-//console.log(10n===10);
-const bigint=1n;
-const num=2;
-//console.log(bigint+BigInt(num));
-console.log(Number(bigint)+num);
+// let user={name:'Ivan'};
+// let map  = new WeakMap();
+// map.set(user,'data');
+// user=null;
+// console.log(map);
+let cache  = new WeakMap();
+function cacheUser(user){
+	if( !cache.has(user)) cache.set(user, Date.now());
+	return cache.get(user);
+}
+
+let lena = {name: 'Elena'};
+let alex = {name: 'Alex'};
+
+cacheUser(lena);
+cacheUser(alex);
+lena=null;
+console.log(cache.has(lena));
+console.log(cache.has(alex));
+
+
+
+//WeakSet methods: has, add, delete
+
+let messages = [
+	{text: 'Hello', from: 'John'},
+	{text: 'World', from: 'Alex'},
+	{text: '...', from: 'M'},
+];
+
+let readMessages = new WeakSet();
+readMessages.add(messages[0]);
+//readMessages.add(messages[1]);
+readMessages.add(messages[0]);
+messages.shift();
+console.log(messages[0]);
+console.log(readMessages.has(messages[0]));
+
+
