6008c69ef1dcd7584f270e03ca82e453649a19a2
Nataliia Martynenko <87510nata@gmail.com>
2023-04-10 17:00:57 +0200

    lesson 69

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -1,4 +1,6 @@
 'use strict';
+
+//tabs
 window.addEventListener('DOMContentLoaded', () =>{
 	const tabs = document.querySelectorAll('.tabheader__item'),
 		tabsContent = document.querySelectorAll('.tabcontent'),
@@ -36,4 +38,51 @@ window.addEventListener('DOMContentLoaded', () =>{
 			});
 		}
 	});
+
+	//timer
+
+
+	const deadLine = '2023-05-11';
+	function getTimeRemaining(endtime){
+		const t = Date.parse(endtime)-Date.now();
+		const days= Math.floor(t/(1000*60*60*24));
+		const hours= Math.floor((t/(1000*60*60)%24));
+		const minutes= Math.floor((t/(1000*60)%60));
+		const seconds= Math.floor((t/(1000)%60));
+		console.log(t);
+		return {
+			'total': t,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds,
+		};
+	}
+
+	function getZero(num){
+		if(num>=0 && num<10) return`0${num}`;
+		return num;
+	}
+	function setClock(selector, endtime){
+		const timer=document.querySelector(selector),
+			days = document.querySelector('#days'),
+			hours = document.querySelector('#hours'),
+			minutes = document.querySelector('#minutes'),
+			seconds = document.querySelector('#seconds'),
+			timeInterval = setInterval(updateClock, 1000);
+		updateClock();
+		function updateClock(){
+			const t = getTimeRemaining(endtime);
+			days.innerHTML = getZero(t.days);
+			hours.innerHTML = getZero(t.hours);
+			minutes.innerHTML = getZero(t.minutes);
+			seconds.innerHTML = getZero(t.seconds);
+			if(t.total <=0){
+				clearInterval(timeInterval);
+			}
+		}
+
+
+	}
+	setClock('.timer', deadLine);
 });
