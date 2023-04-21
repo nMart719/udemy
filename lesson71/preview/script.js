bd20551d0963d00a1fed6735971b998e30a75a9c
Nataliia Martynenko <87510nata@gmail.com>
2023-04-21 19:40:39 +0200

    lesson 70

---
+++ lesson71/preview/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const box = document.querySelector('.box'),
+	btn = document.querySelector('button');
+
+const width = box.scrollWidth;
+const height = box.scrollHeight;
+
+console.log(width);
+console.log(height);
+
+btn.addEventListener('click', ()=>{
+	// box.style.height = box.scrollHeight+'px';
+	console.log(box.scrollTop);
+});
+
+console.log(box.getBoundingClientRect().top);
+
+const style=window.getComputedStyle(box, );
+console.log(style.display);
+
+console.log(document.documentElement.scrollTop);
