cd84b9bc22494f56558864292487952efe005faf
Nataliia Martynenko <87510nata@gmail.com>
2023-04-22 12:16:47 +0200

    lesson 76

--- script.js
+++ script.js
@@ -1,18 +1,84 @@
 'use strict';
-function User(name, id){
-	this.name= name;
-	this.id=id;
-	this.human=true;
-	this.hello = function(){
-		console.log('Hello '+this.name);
-	};
-}
-User.prototype.exit = function(){
-	console.log('User '+ this.name + ' has gone');
+// function showThis(a,b){
+// 	console.log(this);
+// 	function sum(){
+// 		console.log(this);
+// 		return a+b;
+// 	}
+// 	console.log(sum());
+// }
+// showThis(4,5);
+
+// const obj={
+// 	a:20,
+// 	b:15,
+// 	sum: function(){
+// 		function shout(){
+// 			console.log(this);
+// 		}
+// 		shout();
+
+// 	}
+// };
+// obj.sum();
+
+
+// function User(name, id){
+// 	this.name=name,
+// 	this.id=id,
+// 	this.human=true;
+// 	this.hello=function(){
+// 		console.log('Hello! '+ this.name);
+// 	};
+// }
+
+// let ivan = new User('Ivan', 28);
+
+// function sayName(surname){
+// 	console.log(this);
+// 	console.log(this.name + surname);
+// }
+
+// const user={
+// 	name: 'John'
+// };
+
+// sayName.call(user, 'Smith');
+// sayName.apply(user, ['Smith']);
+
+
+// function count(num){
+// 	return this*num;
+// }
+
+// const double = count.bind(2);
+// console.log(double(3));
+// console.log(double(13));
+
+//1) обычная функция: this=window, но если стоит use strict, то будет undefined
+//2) контекст у методов объекта - сам объект
+//3) this в конструкторах и классах - это новый экземпляр объекта
+//4) ручная привязка this: call, reply, bind
+
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click',(e)=>{
+	//console.log(this);
+	e.target.style.backgroundColor='red';
+});
+
+
+let obj = {
+	num: 5,
+	sayNumber: function(){
+		const say = () =>{
+			console.log(this.num);
+		};
+		say();
+	}
 };
-const ivan=new User('Ivan', 28);
-const alex=new User('Alex', 20);
-ivan.exit();
-alex.exit();
-// console.log(ivan);
-// console.log(alex);
+obj.sayNumber();
+
+const double = a=>a*2;
+console.log(double(4));
