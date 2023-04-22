9f68ad1c34f6490e9060c170cb9413109d52f46d
Nataliia Martynenko <87510nata@gmail.com>
2023-04-22 12:30:55 +0200

    lesson 77

--- script.js
+++ script.js
@@ -1,84 +1,35 @@
 'use strict';
-// function showThis(a,b){
-// 	console.log(this);
-// 	function sum(){
-// 		console.log(this);
-// 		return a+b;
-// 	}
-// 	console.log(sum());
-// }
-// showThis(4,5);
 
-// const obj={
-// 	a:20,
-// 	b:15,
-// 	sum: function(){
-// 		function shout(){
-// 			console.log(this);
-// 		}
-// 		shout();
-
-// 	}
-// };
-// obj.sum();
-
-
-// function User(name, id){
-// 	this.name=name,
-// 	this.id=id,
-// 	this.human=true;
-// 	this.hello=function(){
-// 		console.log('Hello! '+ this.name);
-// 	};
-// }
-
-// let ivan = new User('Ivan', 28);
-
-// function sayName(surname){
-// 	console.log(this);
-// 	console.log(this.name + surname);
-// }
-
-// const user={
-// 	name: 'John'
-// };
-
-// sayName.call(user, 'Smith');
-// sayName.apply(user, ['Smith']);
-
-
-// function count(num){
-// 	return this*num;
-// }
-
-// const double = count.bind(2);
-// console.log(double(3));
-// console.log(double(13));
-
-//1) обычная функция: this=window, но если стоит use strict, то будет undefined
-//2) контекст у методов объекта - сам объект
-//3) this в конструкторах и классах - это новый экземпляр объекта
-//4) ручная привязка this: call, reply, bind
-
-
-const btn = document.querySelector('button');
-
-btn.addEventListener('click',(e)=>{
-	//console.log(this);
-	e.target.style.backgroundColor='red';
-});
-
-
-let obj = {
-	num: 5,
-	sayNumber: function(){
-		const say = () =>{
-			console.log(this.num);
-		};
-		say();
+class Rectangle{
+	constructor(height, width) {
+		this.height=height;
+		this.width=width;
 	}
-};
-obj.sayNumber();
 
-const double = a=>a*2;
-console.log(double(4));
+	calcArea(){
+		return this.height*this.width;
+	}
+}
+
+
+class ColoredRectangleWithText extends Rectangle{
+	constructor(height, width, text, bgColor) {
+		super(height, width);
+		this.text=text;
+		this.bgColor=bgColor;
+	}
+	showMyProps(){
+		console.log(`Text: ${this.text}, ${this.bgColor}`);
+	}
+}
+
+const div = new ColoredRectangleWithText(25, 10, 'Hello World!', 'red');
+
+div.showMyProps();
+console.log(div.calcArea());
+
+const square = new Rectangle(10,10);
+const long = new Rectangle(20,100);
+
+// console.log(square.calcArea());
+// console.log(long.calcArea());
