f2cd2f2662fd9dc0cb71c97b0a3b1e7d57186b84
Nataliia <87510nata@gmail.com>
2023-05-01 11:35:32 +0200

    food project deleted

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
 
-'use strict'
+'use strict';
 
-const inputRub=document.querySelector('#rub')
-const inputUsd=document.querySelector('#usd')
+const inputRub=document.querySelector('#rub');
+const inputUsd=document.querySelector('#usd');
 
 inputRub.addEventListener('input',()=>{
-    const request = new XMLHttpRequest();
-    request.open('GET', 'js/current.json');
-    request.setRequestHeader('Content-type','application/json; charset=utf-8');
-    request.send();
-    request.addEventListener('load',()=>{
-        if(request.status === 200){
-            console.log(request.response);
-            const data=JSON.parse(request.response);
-            inputUsd.value=(+inputRub.value/data.current.usd).toFixed(2);
-        }else{
-            inputUsd.value = 'Something went wrong'
-        }
-    });
+	const request = new XMLHttpRequest();
+	request.open('GET', 'js/current.json');
+	request.setRequestHeader('Content-type','application/json; charset=utf-8');
+	request.send();
+	request.addEventListener('load',()=>{
+		if(request.status === 200){
+			console.log(request.response);
+			const data=JSON.parse(request.response);
+			inputUsd.value=(+inputRub.value/data.current.usd).toFixed(2);
+		}else{
+			inputUsd.value = 'Something went wrong';
+		}
+	});
 });
->>>>>>> 9f2b379 (lesson 83 AJAX)
