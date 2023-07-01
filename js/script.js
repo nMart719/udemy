9a67903bccdd058c3a7664c984192594ba79f8f0
Nataliia <87510nata@gmail.com>
2023-07-01 13:28:36 +0200

    lesson 89

--- js/script.js
+++ js/script.js
@@ -282,13 +282,7 @@ window.addEventListener('DOMContentLoaded', () =>{
 			prevModalDialog.classList.remove('hide');
 		},4000);	
 	}
-	fetch('https://jsonplaceholder.typicode.com/posts',{
-		method:'POST',
-		body:JSON.stringify({name:'Alex'}),
-		headers:{
-			'Content-Type': 'application/json'
-		}
-	})
-		.then(response=>response.json())
-		.then(json=>console.log(json));
+	fetch('db.json')
+		.then(data=>data.json())
+		.then(res=>console.log(res));
 });
