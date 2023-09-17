0da28077410bdb004a0f26cf13599edb982d6680
Nataliia <87510nata@gmail.com>
2023-09-17 14:02:50 +0200

    lesson 92

--- js/script.js
+++ js/script.js
@@ -191,25 +191,6 @@ window.addEventListener('DOMContentLoaded', () =>{
 
 		return await res.json();
 	};
-	// getResource('http://localhost:3000/menu')	
-	// 	.then(data=>createCard(data));
-
-	// function createCard(data){
-	// 	data.forEach(({img, altimg, title, descr, price})=>{
-	// 		const element = document.createElement('div');
-	// 		element.classList.add('menu__item');
-	// 		element.innerHTML=`
-	// 		<img src=${img} alt=${altimg}>
-	// 		<h3 class="menu__item-subtitle">${title}</h3>
-	// 		<div class="menu__item-descr">${descr}</div>
-	// 		<div class="menu__item-divider"></div>
-	// 		<div class="menu__item-price">
-	// 			<div class="menu__item-cost">Цена:</div>
-	// 			<div class="menu__item-total"><span>${price}</span> грн/день</div>
-	// 		</div>`;			
-	// 		document.querySelector('.menu .container').append(element);
-	// 	});
-	// }
 	axios.get('http://localhost:3000/menu').then(data=>{
 		data.data.forEach(({img,altimg,title,descr,price})=>{
 			new MenuCard(img,altimg,title,descr,price, '.menu .container').render();
@@ -287,7 +268,39 @@ window.addEventListener('DOMContentLoaded', () =>{
 			prevModalDialog.classList.remove('hide');
 		},4000);	
 	}
-	fetch('db.json')
-		.then(data=>data.json())
-		.then(res=>console.log(res));
+	//Slider
+	const slides = document.querySelectorAll('.offer__slide');
+	const prev = document.querySelector('.offer__slider-prev');
+	const next = document.querySelector('.offer__slider-next');
+	const total = document.querySelector('#total');
+	const current = document.querySelector('#current');
+	let slideIndex = 1;
+	showSlides(slideIndex);
+	
+	function showSlides(n){
+		if(n>slides.length){
+			slideIndex=1;
+		}
+		if(n<1){
+			slideIndex=slides.length;
+		}
+
+		slides.forEach(item=>item.style.display='none');
+		slides[slideIndex-1].style.display='block';
+		
+		if(slides.length < 10) current.textContent=`0${slideIndex}`;
+		else current.textContent=slideIndex;
+	}
+
+	function plusSlides(n){
+		showSlides(slideIndex+=n);
+	}
+
+
+	prev.addEventListener('click',()=>{
+		plusSlides(-1);
+	});
+	next.addEventListener('click',()=>{
+		plusSlides(1);
+	});
 });
