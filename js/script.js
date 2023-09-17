6026a6b31ac17f6e26620c8aa1783d1dee44b3cf
Nataliia <87510nata@gmail.com>
2023-09-17 13:38:58 +0200

    lesson 90

--- js/script.js
+++ js/script.js
@@ -182,37 +182,35 @@ window.addEventListener('DOMContentLoaded', () =>{
 
 		}
 	}
+	const getResource=async (url)=>{
+		const res= await fetch(url);
 
+		if(!res.ok){
+			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+		}
 
+		return await res.json();
+	};
+	getResource('http://localhost:3000/menu')	
+		.then(data=>createCard(data));
 
-	new MenuCard(
-		'img/tabs/vegy.jpg',
-		'vegy',
-		'Меню "Фитнес"',
-		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-		9,
-		'.menu .container',
-		'menu__item',
-		'big'
-	).render();
-
-	new MenuCard(
-		'img/tabs/elite.jpg',
-		'elite',
-		'Меню “Премиум”',
-		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-		14,
-		'.menu .container',
-	).render();
-
-	new MenuCard(
-		'img/tabs/post.jpg',
-		'post',
-		'Меню "Постное"',
-		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ',
-		21,
-		'.menu .container',
-	).render();
+	function createCard(data){
+		data.forEach(({img, altimg, title, descr, price})=>{
+			const element = document.createElement('div');
+			element.classList.add('menu__item');
+			element.innerHTML=`
+			<img src=${img} alt=${altimg}>
+			<h3 class="menu__item-subtitle">${title}</h3>
+			<div class="menu__item-descr">${descr}</div>
+			<div class="menu__item-divider"></div>
+			<div class="menu__item-price">
+				<div class="menu__item-cost">Цена:</div>
+				<div class="menu__item-total"><span>${price}</span> грн/день</div>
+			</div>`;			
+			document.querySelector('.menu .container').append(element);
+		});
+	}
+	
 	//Forms
 	const message={
 		loading: 'img/form/spinner.svg',
@@ -223,9 +221,20 @@ window.addEventListener('DOMContentLoaded', () =>{
 
 	const forms=document.querySelectorAll('form');
 	forms.forEach(item =>{
-		postData(item);
+		bindPostData(item);
 	});
-	function postData(form){
+
+	const postData=async (url,data)=>{
+		const res= await fetch(url,{
+			method: 'POST',
+			headers:{
+				'Content-type':'application/json'
+			},
+			body: data
+		});
+		return await res.json();
+	};
+	function bindPostData(form){
 		form.addEventListener('submit',(e)=>{
 			e.preventDefault();
 
@@ -238,18 +247,10 @@ window.addEventListener('DOMContentLoaded', () =>{
 			form.insertAdjacentElement('afterend', statusMessage);
 			
 			const formData = new FormData(form); 
-			const object={};
-			formData.forEach(function(value, key){
-				object[key]=value;
-			});
 
-			fetch('server.php', {
-				method: 'POST',
-				headers:{
-					'Content-type':'application/json'
-				},
-				body: JSON.stringify(object)
-			}).then(data=>data.text())
+			
+			const json=JSON.stringify(Object.fromEntries(formData.entries()));
+			postData('http://localhost:3000/requests', json)
 				.then(data=>{
 					console.log(data);
 					showThanksModal(message.success);
